ed0a3b33bbf72f6e8f2d0d72bc52deaf6dd357c7
Dexter V <dvubox@gmail.com>
2013-06-13 16:52:58 -0700

    WIP create test for user model and integration test for user sign up

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'User' do
+  context 'signing up' do
+    before { visit root_path }
+
+    context 'with valid information' do
+      describe 'when all inputs are filled in' do
+        it 'will create a new account' do
+          pending
+        end
+      end
+    end
+
+    context 'with invalid information' do
+      describe 'such as password' do
+        it 'will return errors' do
+          pending
+        end
+
+      describe 'such as email' do
+        it 'will return errors' do
+          pending
+        end
+      end
+    end
+
+    context 'without any information' do
+      it 'will return errors' do
+        pending
+      end
+    end
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,24 @@ describe 'User' do
   context 'when created' do
     describe 'with required valid inputs' do
       it 'will create user' do
-        user = User.create(first_name: 'Dexter', last_name: 'Vu', username: 'dextervu', password: 'password')
+        user = User.create( first_name: 'Dexter',
+                            last_name: 'Vu',
+                            email: 'dex@dex.com',
+                            username: 'dextervu',
+                            password: 'password')
         expect(user.valid?).to be_true
+      end
+    end
+
+    describe 'without any inputs' do
+      it 'will not create user' do
+        user = User.create( first_name: '',
+                            last_name: '',
+                            email: '',
+                            username: '',
+                            password: '')
+
+        expect(user.invalid?).to be_true
       end
     end
   end
