c24f670b0e091341449e1cbe8e66d303897d4c8e
Dexter V <dvubox@gmail.com>
2013-06-13 12:51:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daddyshop::Application.config.secret_token = '3b9a850c6ea9e321278ce6d174dcbc87376e031328f70ee235c00e8639890f72e7cb1000ebb3159bdb0e4ab48f886a9e627004391443697d5bf51d4cfbfcc985'
