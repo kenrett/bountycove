3e4a90a963f07b1b1515a5e59ac6ec4531da6a8d
Dexter V <dvubox@gmail.com>
2013-06-14 12:01:43 -0700

    create sti relationship for parent and child create models for parent and child add name assignment functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :username, :email, :password, :password_confirmation
+  attr_accessible :first_name, :last_name, :username, :email, :password, :password_confirmation, :name
 
   validates :username, :uniqueness => true
-  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+  validates :email,
+            :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+                         :on => :create },
+            :if => :parent?
   validates :password, :presence => true, :on => :create
 
   has_secure_password
@@ -11,4 +14,10 @@ class User < ActiveRecord::Base
     first_name + " " + last_name
   end
 
+  def name=(name)
+    full_name       = name.gsub(/\W/, ' ').split
+
+    self.first_name = full_name[0..-2].join(' ')
+    self.last_name  = full_name.last
+  end
 end
