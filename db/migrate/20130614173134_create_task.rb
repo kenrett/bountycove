1508fe4381918789a984fffd083ff9a87bb05464
Apprentice <apprentice@devbootcamp.com>
2013-06-14 20:08:30 -0700

    change foreign key name to captain_id

--- db/migrate/20130614173134_create_task.rb
+++ db/migrate/20130614173134_create_task.rb
@@ -1,7 +1,7 @@
 class CreateTask < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.integer :worth, :parent_id #user_id?
+      t.integer :worth, :captain_id
       t.string :name, :description
       t.timestamps
     end
