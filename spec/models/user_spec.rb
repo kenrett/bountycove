68ab0a6cb95a1e7b395466688017d9f66b736ea0
travisl12 <travis.lawrence12@gmail.com>
2013-06-16 13:29:10 -0700

    Add pirate user model tests, all green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'User' do
+describe 'Captain' do
   context 'when created' do
     describe 'with required valid inputs' do
       it 'will create user' do
@@ -47,6 +47,76 @@ describe 'User' do
     describe 'without any inputs' do
       it 'will not create user' do
         user = Captain.create( first_name: '',
+                            last_name: '',
+                            email: '',
+                            username: '',
+                            password: '')
+
+        expect(user.invalid?).to be_true
+      end
+    end
+  end
+end
+
+describe 'Pirate' do
+  context 'when created' do
+    describe 'with required valid inputs' do
+      it 'will create user' do
+        user = Pirate.create( first_name: 'Dexter',
+                            last_name: 'Vu',
+                            email: 'dex@dex.com',
+                            username: 'dextervu',
+                            password: 'password')
+
+        expect(user.valid?).to be_true
+      end
+    end  
+
+    describe 'with duplicate username' do
+      it 'will not create user' do
+       Pirate.create(  first_name: 'Ken',
+                      last_name: 'Kenny',
+                      email: 'kenny@kenster.com',
+                      username: 'kennyboy',
+                      password: 'password')
+
+        user2 = Pirate.create(  first_name: 'Ken',
+                              last_name: 'Kenny',
+                              email: 'kenny@kenster.com',
+                              username: 'kennyboy',
+                              password: 'password')
+
+        expect(user2.invalid?).to be_true
+      end
+    end
+
+    describe 'with no email address' do
+      it 'will create user' do
+        user = Pirate.create( first_name: 'Ken',
+                            last_name: 'Kenny',
+                            email: '',
+                            username: 'kennyboy',
+                            password: 'password')
+
+        expect(user.valid?).to be_true
+      end
+    end    
+
+    describe 'with invalid email address' do
+      it 'will create user' do
+        user = Pirate.create( first_name: 'Ken',
+                            last_name: 'Kenny',
+                            email: 'd',
+                            username: 'kennyboy',
+                            password: 'password')
+
+        expect(user.invalid?).to be_true
+      end
+    end
+
+    describe 'without any inputs' do
+      it 'will not create user' do
+        user = Pirate.create( first_name: '',
                             last_name: '',
                             email: '',
                             username: '',
