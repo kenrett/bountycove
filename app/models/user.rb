d5b42e4ce486be79f6b77927188da3ddd31e9248
Apprentice <apprentice@devbootcamp.com>
2013-06-16 16:29:56 -0700

    Added validations for presence for first and last_name 	modified:   app/models/user.rb
    Wrote a bunch of tests for the model!
    	modified:   spec/models/user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :username, :email, :password, :password_confirmation, :name
 
-  validates :username, :uniqueness => true
+  validates :username, :uniqueness => true, :presence => true
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
   validates :email,
             :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
                          :on => :create },
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,52 +5,128 @@ describe 'Captain' do
     describe 'with required valid inputs' do
       it 'will create user' do
         user = Captain.create( first_name: 'Dexter',
-                            last_name: 'Vu',
-                            email: 'dex@dex.com',
-                            username: 'dextervu',
-                            password: 'password')
+                               last_name: 'Vu',
+                               email: 'dex@dex.com',
+                               username: 'dextervu',
+                               password: 'password')
 
         expect(user.valid?).to be_true
+      end
+
+      it 'should have_many pirates' do
+        t = Captain.reflect_on_association(:pirates)
+        t.macro.should == :has_many
+      end
+
+      it 'should have_many treasures' do
+        t = Captain.reflect_on_association(:treasures)
+        t.macro.should == :has_many
+      end
+
+      it 'should have tasks' do
+        t = Captain.reflect_on_association(:tasks)
+        t.macro.should == :has_many
       end
     end
 
     describe 'with duplicate username' do
       it 'will not create user' do
        Captain.create(  first_name: 'Dexter',
-                      last_name: 'Vu',
-                      email: 'dex@dex.com',
-                      username: 'dextervu',
-                      password: 'password')
+                        last_name: 'Vu',
+                        email: 'dex@dex.com',
+                        username: 'dextervu',
+                        password: 'password')
 
-        user2 = Captain.create(  first_name: 'Dexter',
-                              last_name: 'Vu',
-                              email: 'dex@dex.com',
-                              username: 'dextervu',
-                              password: 'password')
+       user2 = Captain.create(  first_name: 'Dexter',
+                                last_name: 'Vu',
+                                email: 'dex@dex.com',
+                                username: 'dextervu',
+                                password: 'password')
 
-        expect(user2.invalid?).to be_true
-      end
-    end
+       expect(user2.invalid?).to be_true
+     end
+   end
 
     describe 'with invalid email address' do
       it 'will not create user' do
         user = Captain.create( first_name: 'Dexter',
-                            last_name: 'Vu',
-                            email: 'd',
-                            username: 'dextervu',
-                            password: 'password')
+                               last_name: 'Vu',
+                               email: 'd',
+                               username: 'dextervu',
+                               password: 'password')
+
+      expect(user.invalid?).to be_true
+      end
+    end
+
+    describe 'with no first_name' do
+      it 'will not create user' do
+        user = Captain.create( first_name: '',
+                               last_name: 'Vu',
+                               email: 'dex@dex.com',
+                               username: 'dextervu',
+                               password: 'password')
 
         expect(user.invalid?).to be_true
       end
     end
 
+    describe 'with no last_name' do
+      it 'will not create user' do
+        user = Captain.create( first_name: 'Dexter',
+                               last_name: '',
+                               email: 'dex@dex.com',
+                               username: 'dextervu',
+                               password: 'password')
+
+        expect(user.invalid?).to be_true
+      end
+    end
+
+    describe 'with no email address' do
+      it 'will not create user' do
+        user = Captain.create( first_name: 'Dexter',
+                               last_name: 'Vu',
+                               email: '',
+                               username: 'dextervu',
+                               password: 'password')
+
+        expect(user.invalid?).to be_true
+      end
+    end
+
+    describe 'with no username' do
+      it 'will not create user' do
+        user = Captain.create( first_name: 'Dexter',
+                               last_name: 'Vu',
+                               email: 'dex@dex.com',
+                               username: '',
+                               password: 'password')
+
+        expect(user.invalid?).to be_true
+      end
+    end
+
+    describe 'with no password' do
+      it 'will not create user' do
+        user = Captain.create( first_name: 'Dexter',
+                               last_name: 'Vu',
+                               email: 'dex@dex.com',
+                               username: 'dextervu',
+                               password: '')
+
+        expect(user.invalid?).to be_true
+      end
+    end
+
+
     describe 'without any inputs' do
       it 'will not create user' do
         user = Captain.create( first_name: '',
-                            last_name: '',
-                            email: '',
-                            username: '',
-                            password: '')
+                               last_name: '',
+                               email: '',
+                               username: '',
+                               password: '')
 
         expect(user.invalid?).to be_true
       end
