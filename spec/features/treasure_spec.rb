64616bf8ca6c6ecc5965b45e01f8d08a3d5b9207
Apprentice <apprentice@dbc01.local>
2013-06-15 20:24:28 -0700

    treasure rspec wip

--- spec/features/treasure_spec.rb
+++ spec/features/treasure_spec.rb
@@ -1,39 +1,44 @@
 require 'spec_helper'
 
-describe 'User Profile' do
-  context 'when signing in' do
-   it 'will redirect to user profile page' do
-    captain = FactoryGirl.create(:captain, :first_name => "Adam", :username => 'acsart')
-    visit '/captains/acsart/'
-    expect(page).to have_selector("p", :text => "Adam")
-    end
-  end
-
-  context 'when on profile page' do
-    it 'will have option to add pirate, add loot, add event and create adventure' do
-      captain = FactoryGirl.create(:captain, :username => 'acsart')
-      visit '/captains/acsart/'
-      expect(page).to have_link("Add Pirate")
-      expect(page).to have_link("Add Loot")
-      expect(page).to have_link("Create Adventure")
-      expect(page).to have_link("Add Event")
-    end
-  end
-  
-  context 'when on profile page' do
-    it 'will redirect to add treasure when click add loot' do
+describe 'Treasure' do
+  context 'adding loot' do
+    it 'will create a new treasure' do
+    before do
       captain = FactoryGirl.create(:captain, :username => 'acsart')
       visit '/'
       fill_in 'username', with: captain.username
       fill_in 'password', with: captain.password
       click_button 'Login!' 
+    end
       visit '/captains/acsart/treasures'
-      expect(page).to have_selector("div.create_treasure_form")
-      expect(page).to have_selector("form")
-      expect(page).to have_selector("input")
+      fill_in 'name', with: 'test treasure'
+      fill_in 'description', with: 'test description'
+      fill_in 'photo', with: 'test photo'
+      fill_in 'price', with: '5'
+      click_button 'Create Treasure'
+      expect(Treasure.all.count).to change_by(1)
+      end      
     end
   end
-end
+
+  # context 'when on profile page' do
+  #   it 'will have option to add pirate, add loot, add event and create adventure' do
+  #     visit '/captains/acsart/'
+  #     expect(page).to have_link("Add Pirate")
+  #     expect(page).to have_link("Add Loot")
+  #     expect(page).to have_link("Create Adventure")
+  #     expect(page).to have_link("Add Event")
+  #   end
+  # end
+  
+  # context 'when on profile page' do
+  #   it 'will redirect to add treasure when click add loot' do
+  #     captain = FactoryGirl.create(:captain, :username => 'acsart')
+  #     visit '/'
+  #     expect(page).to have_selector("div.create_treasure_form")
+  #     expect(page).to have_selector("form")
+  #     expect(page).to have_selector("input")
+
 
 
 
