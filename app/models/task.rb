c86025145b321fef77dd8d6bd5d8f1b4e1bcf8c6
Dexter V <dvubox@gmail.com>
2013-06-16 21:25:18 -0700

    add constances to represent state of status codes for task and treasure models fix treasure's price validation to only validate for ON_SALE create only update pirate index view to display different types of treasures: on_sale, bought, and wishlist

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,7 +10,13 @@ class Task < ActiveRecord::Base
   validates :description, :format => { :with => /\w+/ }
   validates :worth, :presence => true, :numericality => { :only_integer => true }
 
-  STATUS = {on_board: 1, assigned: 2, completed: 3}
+  ON_BOARD  = 1
+  ASSIGNED  = 2
+  COMPLETED = 3
+
+  STATUS = { on_board: ON_BOARD,
+             assigned: ASSIGNED,
+             completed: COMPLETED}
 
   STATUS.each do |key, value|
     define_method("#{key.to_s}?") { status == value }
@@ -19,5 +25,5 @@ class Task < ActiveRecord::Base
 
   def string?
 
-  end 
+  end
 end
