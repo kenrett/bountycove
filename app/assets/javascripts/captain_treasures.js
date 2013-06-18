ccc581b10c650cb774588ad6437df82db65d9376
Apprentice <apprentice@devbootcamp.com>
2013-06-17 21:00:32 -0700

    add ajax script to dynamically change captain homepage change captains#show to ajax the homepage add link to ajax the page to show captain index page

---
+++ app/assets/javascripts/captain_treasures.js
@@ -0,0 +1,28 @@
+function List(elem,title,content) {
+  this.elem = elem;
+  this.title = title;
+  this.content = content;
+}
+
+List.prototype = {
+  renderToPage: function() {
+    this.renderTemplate(this.title,this.content);
+
+    $(this.elem).html(this.template);
+  },
+
+  renderTemplate: function(header, content) {
+    this.template = "<ul class='treasures_on_sale'><h1>" +header+ "</h1>" +content+ "</ul>";
+  }
+}
+
+$(document).ready(function(){
+  $('.captain_treasure_index').on('ajax:success', function(e, data, status, xhr){
+    leftBox = new List('.captain_profile_left', 'Treasure on sale', data.treasures_on_sale);
+    rightBox = new List('.captain_profile_right', 'Treasure bought', data.treasures_bought);
+    botBotx = new List('')
+    leftBox.renderToPage();
+    rightBox.renderToPage();
+  });//end on
+  
+});//end ready
