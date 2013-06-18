fc7bc0502d752af1c51445332a5ea60716d22e84
dbcapprentice <apprentice@dbc21.local>
2013-06-17 18:16:01 -0700

    Put max limit on Tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,11 +3,11 @@ module TasksHelper
   def count_of_tasks_need_verify
     Task.count(:conditions => "status = 3")
   end
-  
+
   def count_of_available_tasks
     Task.count(:conditions => "status = 1")
   end
-  
+
   def time_format(task)
     if task.updated_at.strftime('%m/%d/%Y') == Date.today.strftime('%m/%d/%Y')
       '%l:%M %P'
