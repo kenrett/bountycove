8af4d0a2146e494ccf91cf722e7a6701cd3ce8c6
dbcapprentice <apprentice@dbc21.local>
2013-06-19 16:13:45 -0700

    remove old js file

--- app/assets/javascripts/captain_task_complete.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $("#fun").on("ajax:success", function(event, data, s, xhr){
-    console.log(data);
-    debugger;
-    $(this).closest(parent).remove();
-  });
-});
