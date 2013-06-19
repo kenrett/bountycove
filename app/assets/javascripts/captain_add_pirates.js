632fe6b18e605238750b92dfef5c50222ac846b1
Dexter V <dvubox@gmail.com>
2013-06-19 13:27:24 -0700

    ajax pirate new signup success message to be a foundation message

--- app/assets/javascripts/captain_add_pirates.js
+++ app/assets/javascripts/captain_add_pirates.js
@@ -14,6 +14,22 @@ PirateError.prototype = {
   }
 };
 
+function PirateSuccess(elem, message) {
+  this.elem = elem;
+  this.message = message;
+}
+
+PirateSuccess.prototype = {
+  renderToPage: function() {
+    this.createTemplate(this.message);
+    $(this.elem).html(this.template);
+  },
+
+  createTemplate: function(message) {
+    this.template = "<div data-alert class='alert-box'>"+message+"<a href='#' class='close'>&times;</a></div>";
+  }
+};
+
 $(document).ready(function() {
   // Renders pirate account signup form for captain
   $('.captain.profile_left').on('ajax:success', '#add_pirate', function(e, data, status, xhr) {
@@ -24,7 +40,9 @@ $(document).ready(function() {
   $('.captain.profile_right').on('ajax:success', '#new_pirate', function(e, data, status, xhr) {
     $('.captain.profile_left').html(data.list_of_pirates);
     $('.captain.profile_right').html(data.sign_up_form);
-    $('.captain.profile_right #new_pirate_message').html(data.success_message);
+
+    var pirateSuccessMessage = new PirateSuccess('.captain.profile_right #new_pirate_message', data.success_message);
+    pirateSuccessMessage.renderToPage();
   }).on('ajax:error', '#new_pirate', function(e, data, status, xhr) {
     var error = '';
     data.responseJSON.forEach(function(value, index) {
