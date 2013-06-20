1fcbfaec9a9919672db08756f832a01135fc996f
dbcapprentice <apprentice@dbc21.local>
2013-06-19 19:04:04 -0700

    rm old files

--- app/assets/javascripts/pirate_task.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".accept_quest a").on("ajax:success", function(event, data, s, xhr){
-    for (var x in data){
-      $(".flash_message").html(data[x]);
-    };
-    $(this).closest('ul').remove();
-  });
-});
