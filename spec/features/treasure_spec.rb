dc0175813c2356d828448236d68ca37d3292b037
Ken Rettberg <kenrettberg@gmail.com>
2013-06-19 20:59:50 -0700

    updating

--- spec/features/treasure_spec.rb
+++ spec/features/treasure_spec.rb
@@ -1,28 +1,40 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe 'Treasure' do
-#   before do
-#     captain = FactoryGirl.create(:captain, :username => 'acsart')
-#     @treasure = FactoryGirl.create(:treasure)
-#     visit '/'
-#     fill_in 'username', with: captain.username
-#     fill_in 'password', with: captain.password
-#     click_button 'Login!' 
-#   end
-#   context 'adding loot' do
-#     it 'will create a new treasure' do
-#       visit '/captains/acsart/treasures'
-#       fill_in 'treasure_name', with: @treasure.name
-#       fill_in 'treasure_description', with: @treasure.description
-#       fill_in 'treasure_photo', with: @treasure.photo
-#       fill_in 'treasure_price', with: @treasure.price
-#       click_button 'Create Treasure'
+describe 'Treasure' do
+  
+  context 'creating treasure' do
+    let(:captain) { build(:captain) }
+    let(:treasure) { build(:treasure) }
 
-#       expect{ }.to change(Treasure, :count).by(1)
-      
-#       end      
-#     end
-#   end
+    before do
+      visit root_path
+
+      fill_in 'sign_up captain_first_name', with: captain.first_name
+      fill_in '#big captain_last_name', with: captain.last_name
+      fill_in '#big captain_username', with: captain.username
+      fill_in '#big captain_email', with: captain.email
+      fill_in '#big captain_password', with: captain.password
+      fill_in '#big captain_password_confirmation', with: captain.password_confirmation
+      click_button 'Sign Up'
+    end
+
+    context 'with valid information' do
+      describe 'when all inputs are filled' do
+        it 'will create a new treasure' do
+          visit new_captain_treasure_path(captain)
+          fill_in 'treasure_name', with: treasure.name
+          fill_in 'treasure_description', with: treasure.description
+          fill_in 'treasure_photo', with: treasure.photo
+          fill_in 'treasure_price', with: treasure.price
+
+          expect{
+            click_button 'Create Treasure' 
+            }.to change(Treasure, :count).by(1)
+        end
+      end      
+    end
+  end
+end
 
 
 
